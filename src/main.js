22890856106fc4f425c25a8af07075888f9a6424
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 22:51:22 -0300

    feat: expiration date mask

--- src/main.js
+++ src/main.js
@@ -29,3 +29,22 @@ const securityCodePattern = {
 };
 
 const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+const expirationDate = document.querySelector('#expiration-date');
+const expirationDatePattern = {
+  mask: 'MM{/}YY',
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+};
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
